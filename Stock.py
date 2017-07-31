ec42e57f72b788ffff5177b968477b565eece472
Kevin Hsu <kevin.hsu@ite.com.tw>
2017-07-31 11:40:10 +0800

    1. dict has no order 2. trigger api every 5 second 3. show the date and price

--- Stock.py
+++ Stock.py
@@ -1,42 +1,53 @@
 # -*- coding: utf-8 -*-
 import time
+import collections
 import requests
 from bs4 import BeautifulSoup
 
 headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
 Stock = {2330}
-
-Title = {u"時間":0, u"買進":0, u"賣出":0, u"成交價":0, u"漲跌":0}
+Title = [u'買進  ', u'賣出  ', u'成交價', u'漲跌  ']
 
 def HtmlGet():
 	Data = requests.get('https://tw.stock.yahoo.com/q/ts?s=2330', headers = headers)
-#	if Data.status_code == 200:
-#		print 'Get Method Successful'
-#	else:
-#		print 'Get Method Failed. Status code = ', Data.status_code
+	#
+	# response status code 200 = succcess
+	#
+	if Data.status_code == 200:
+		print 'Get Method Successful'
+	else:
+		print 'Get Method Failed. Status code = ', Data.status_code
+		exit()
 	return Data
 
 def DataBs4(Data):
 	Soup = BeautifulSoup(Data, "html.parser")
 	return Soup
 
+def GetCurrentDate():
+	localtime = time.asctime(time.localtime(time.time()))
+	return localtime
 
 def main():
-	# use Get method to get data by send API
-	Context = HtmlGet()
+	while(1):
+		# use Get method to get data by send API
+		Context = HtmlGet()
 
-	# use beautifulsoup to clean up the Context
-	Soup = DataBs4(Context.text)
+		# use beautifulsoup to clean up the Context
+		Soup = DataBs4(Context.text)
 
-	localtime = time.asctime( time.localtime(time.time()) )
-	print localtime
-	# print title
-	for i in Title:
-		print i, Title[i]
+		# To get the curren Time/Date
+		localtime = GetCurrentDate()
+		print u'時間   : ', localtime
 
+		# print title and price
+		index = 0
+		for i in Title:
+			print i,': ', Soup.select(".high")[index].text
+			index = index+1
 
-	print Soup.select(".high")[0].text, Soup.select(".high")[1].text, Soup.select(".high")[2].text, Soup.select(".high")[3].text
-
+		# wait 5 second
+		time.sleep(5)
 
 if __name__ == '__main__':
 	main()
